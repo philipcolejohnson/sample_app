b18ec90d0cb5dedb093a3c47142b414ddbeee321
Philip Johnson <philipcolejohnson@gmail.com>
2016-06-06 00:16:24 +0000

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
